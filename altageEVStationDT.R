5c54cf9f4edb2db59f1b916613e0c05823a16d44
akbazuka <49175807+akbazuka@users.noreply.github.com>
2019-10-24 11:47:33 -1000

    Add files via upload
    Decision Tree Implemented

---
+++ altageEVStationDT.R
@@ -0,0 +1,86 @@
+require(tree)
+library(lubridate)
+library(rpart)
+library(rpart.plot)
+
+remove_outliers <- function(x, na.rm = TRUE, ...) {
+  qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
+  H <- 1.5 * IQR(x, na.rm = na.rm)
+  y <- x
+  y[x < (qnt[1] - H)] <- NA
+  y[x > (qnt[2] + H)] <- NA
+  y
+}
+
+edata <- read.csv('/Users/akbazuka/Desktop/HACC\ 2019\ Electic/Data_HACC.csv', header=T)
+
+#Start.Time Formatted to add columns MonthOfYear, DayOfWeek, and HoursOfDay
+edata$Start.Time <- mdy_hms(edata$Start.Time) #Format Start.Time Column
+MonthOfYear <- factor(month(edata$Start.Time))
+DayOfWeek <- factor(wday(edata$Start.Time))
+HourOfDay <- factor(hour(edata$Start.Time))
+
+head(HourOfDay)
+#hist(edata$Energy.kWh.)
+
+#Convert Duration column to num in minutes
+edata$Duration <- as.numeric(hms(edata$Duration)) #requires lubridate library; converts duration to seconds
+edata$Duration <- (edata$Duration/60) #Divide by 60 to convert seconds to minutes
+summary(remove_outliers(edata$Duration)) #Calculates summary of vector without outliers
+
+#Convert session amount column to numerical data
+edata$Session.Amount <- as.numeric(gsub("\\$", "", edata$Session.Amount))
+summary(remove_outliers(edata$Session.Amount)) #Calculates summary of vector without outliers
+
+#Make continuous variables classifiable (Use mean numbers gotten from summaries to detemine if high or low)
+Energy <- ifelse(edata$Energy.kWh. <= 13.00, "Energy Less than 13kWh", "Energy More than 13kWh") #Decision Variable becomes whether Energy is HighEnergyer than 13.00 kWh or not
+#Payment <- ifelse(edata$Payment.Mode == "CREDITCARD", 1, 2)
+DurationMin <- ifelse(edata$Duration <= 21.927, "< 22 Min", "> 22 min")
+Amount <- ifelse(edata$Session.Amount <= 4.804, "< $4.8", "> $4.8")
+
+TimeOfDay <- ifelse(HourOfDay == "0", "Off-Peak",
+                    ifelse(HourOfDay == "1", "Off-Peak",
+                           ifelse(HourOfDay == "2", "Off-Peak",
+                                  ifelse(HourOfDay == "3", "Off-Peak",
+                                         ifelse(HourOfDay == "4", "Off-Peak",
+                                                ifelse(HourOfDay == "5", "Off-Peak",
+                                                       ifelse(HourOfDay == "6", "Off-Peak",
+                                                              ifelse(HourOfDay == "7", "Off-Peak",
+                                                                     ifelse(HourOfDay == "8","Mid-Day",
+                                                                            ifelse(HourOfDay == "9", "Mid-Day",
+                                                                                   ifelse(HourOfDay == "10", "Mid-Day",
+                                                                                          ifelse(HourOfDay == "11","Mid-Day",
+                                                                                                 ifelse(HourOfDay == "12", "Mid-Day",
+                                                                                                        ifelse(HourOfDay == "13", "Mid-Day",
+                                                                                                               ifelse(HourOfDay == "14","Mid-Day",
+                                                                                                                      ifelse(HourOfDay == "15", "Mid-Day",
+                                                                                                                             ifelse(HourOfDay == "16", "On-Peak",
+                                                                                                                                    ifelse(HourOfDay == "17","On-Peak",
+                                                                                                                                           ifelse(HourOfDay == "18", "On-Peak",
+                                                                                                                                                  ifelse(HourOfDay == "19", "On-Peak",
+                                                                                                                                                         ifelse(HourOfDay == "20", "On-Peak",
+                                                                                                                                                                ifelse(HourOfDay == "21" ,"Of-Peak",
+                                                                                                                                                                       ifelse(HourOfDay == "22", "Off-Peak","Off-Peak")))))))))))))))))))))))
+
+#Add pre-processed grouped colums together
+edata <- data.frame(edata, Energy, DurationMin, Amount, MonthOfYear, DayOfWeek, TimeOfDay)
+
+#Remove unnecessary columns from dataset before implementing Decision Tree
+edata <- edata[-c(2,3,4,5,7,8,9,10)]
+
+str(edata)
+
+#Implement Decision Tree
+tree.edata = tree(Energy~.-Energy.kWh., data=edata)
+summary(tree.edata)
+#Plot tree
+plot(tree.edata)
+text(tree.edata, use.n=TRUE, all=TRUE, cex=.7, pretty=0)
+
+tree.edata #Detailed Summary of tree
+
+#Implement Decision Tree with rpart package without Energy.kWh column
+fit <- rpart(Energy~.-Energy.kWh., data=edata)
+#Plot
+rpart.plot(fit, extra = "auto")
+summary(fit)
